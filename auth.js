8e34ec20309b7515a845fd928b78bc335ebab0c4
LyueNN244 <lolimus234@outlook.com>
2026-05-17 14:27:49 +0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 const supabaseUrl = 'https://hvsilwppchtpaghqhzcy.supabase.co';
 
-const supabaseKey = 'BURAYA_ANON_PUBLIC_KEY';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2c2lsd3BwY2h0cGFnaHFoemN5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5ODI3MDcsImV4cCI6MjA5NDU1ODcwN30.iVREIgMlleFcyRVRQZiet8Xmvzr-nKXTha4UgiFlzV4';
 
 const client = supabase.createClient(supabaseUrl, supabaseKey);
 
